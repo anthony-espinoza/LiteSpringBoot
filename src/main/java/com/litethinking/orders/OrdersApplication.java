671b20f61e83d92b60db8ef92ce790a9bb00f0da
Anthony Espinoza <ahernan.espinozab@gmail.com>
2026-02-20 13:50:12 -0500

    chore: bootstrap spring boot project (gradle)

---
+++ src/main/java/com/litethinking/orders/OrdersApplication.java
@@ -0,0 +1,13 @@
+package com.litethinking.orders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdersApplication.class, args);
+	}
+
+}
